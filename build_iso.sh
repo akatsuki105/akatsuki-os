7bda7dbfca67e4f4fa69711cbc331890eb363057
Akatsuki-py <bluejapan73+dev@gmail.com>
2019-07-01 23:26:00 +0900

    fix grub filepath bug

--- build_iso.sh
+++ build_iso.sh
@@ -1,4 +1,4 @@
-mkdir -p boot/grub
-cp grub.cfg boot/grub
-cp src/akatsuki.elf boot/
-grub-mkrescue -o os.iso boot/
+mkdir -p isofiles/boot/grub
+cp grub.cfg isofiles/boot/grub
+cp src/akatsuki.elf isofiles/boot/
+grub-mkrescue -o os.iso isofiles/
