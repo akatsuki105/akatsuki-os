08871690342385c96f4bd63d056442b8240cae02
akatsuki-py <bluejapan73+dev@gmail.com>
2019-07-04 15:35:11 +0900

    add asciicode from keyboard to display

--- src/boot.c
+++ src/boot.c
@@ -17,10 +17,21 @@ void display_char(char c, unsigned char foreColor, unsigned char backColor, int 
 void display_cursor(unsigned short pos);
 unsigned char read_keyboard(void);
 
+char keytable[0x80] = {
+    0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0x08, 0,
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '@', '[', 0x0a, 0, 'A', 'S',
+    'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', ':', 0,   0,   ']', 'Z', 'X', 'C', 'V',
+    'B', 'N', 'M', ',', '.', '/', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
+    '2', '3', '0', '.', 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0x5c, 0,  0,   0,   0,   0,   0,   0,   0,   0,   0x5c, 0,  0
+};
+
 void cstart(unsigned long magic, multiboot_info_t *info)
 {
     int i;
-    unsigned char key_code;
+    unsigned char scan_code, ascii_code;
     char *hello_msg = "Starting Akatsuki OS!";
     char *memory_size = "Memory fuck!";
 
@@ -34,8 +45,9 @@ void cstart(unsigned long magic, multiboot_info_t *info)
 
     i = 0;
     for (;;) {
-        key_code = (char)(read_keyboard());
-        display_char(key_code, DEF_COLOR_WHITE, DEF_COLOR_BLACK, i, 2);
+        scan_code = (read_keyboard());
+        ascii_code = keytable[scan_code];
+        display_char(ascii_code, DEF_COLOR_WHITE, DEF_COLOR_BLACK, i, 2);
     }
 }
 
