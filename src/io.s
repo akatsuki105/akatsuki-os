65d92c88cfea2fbbee09c08c1552704785d36605
akatsuki-py <bluejapan73+dev@gmail.com>
2019-07-04 15:34:46 +0900

    add cli sti hlt instruction

--- src/io.s
+++ src/io.s
@@ -1,9 +1,12 @@
 .file "io.s"
 .text
 .align  4
-.globl  inb, outb
+.globl  inb, outb, io_hlt, io_stihlt, io_cli
 .type   inb, @function
 .type   outb, @function
+.type   io_hlt, @function
+.type   io_stihlt, @function
+.type   io_cli, @function
 
 inb:
     movw 4(%esp), %dx
@@ -14,4 +17,17 @@ outb:
     movb 8(%esp), %al # al = data
     movw 4(%esp), %dx # dx = port
     out %al, %dx # data => port
+    ret
+
+io_hlt:
+    hlt
+    ret
+
+io_stihlt:
+    sti
+    hlt
+    ret
+
+io_cli:
+    cli
     ret
