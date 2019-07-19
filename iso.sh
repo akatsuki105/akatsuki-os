16c9e9805d8b75c364a40d4f1bc5415a7fce3c18
akatsuki-py <bluejapan73+dev@gmail.com>
2019-07-19 21:20:48 +0900

    rename  os from myos to akatsuki

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/akatsuki.kernel isodir/boot/akatsuki.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "akatsuki" {
+	multiboot /boot/akatsuki.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o akatsuki.iso isodir
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -4,5 +4,5 @@
 
 void kernel_main(void) {
 	terminal_initialize();
-	printf("Hello, kernel World!\n");
+	printf("Hello, Akatsuki World!\n");
 }
