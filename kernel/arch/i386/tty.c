52fee5971f7d09ec9c8a6d3c83ca8e00fd014080
akatsuki-py <bluejapan73+dev@gmail.com>
2019-07-21 15:38:00 +0900

    add new line process

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -40,6 +40,13 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 
 void terminal_putchar(char c) {
 	unsigned char uc = c;
+	if (uc == '\n') {
+		terminal_column = 0;
+		if (++terminal_row == VGA_HEIGHT) {
+			terminal_row = 0;
+		}
+		return;
+	}
 	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
