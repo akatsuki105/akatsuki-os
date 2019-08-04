8cef67993713a4530a49b86b61f603398583e8ba
akatsuki-py <bluejapan73+dev@gmail.com>
2019-08-04 20:54:41 +0900

    insert sample file into os disk file

--- iso.sh
+++ iso.sh
@@ -5,11 +5,16 @@ set -e
 mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
+mkdir -p isodir/modules
 
 cp sysroot/boot/akatsuki.kernel isodir/boot/akatsuki.kernel
+cp app/sample1.c isodir/modules/sample1.c
+cp app/sample2.c isodir/modules/sample2.c
 cat > isodir/boot/grub/grub.cfg << EOF
 menuentry "akatsuki" {
 	multiboot /boot/akatsuki.kernel
+	module /modules/sample1.c
+	module /modules/sample2.c
 }
 EOF
 grub-mkrescue -o akatsuki.iso isodir
